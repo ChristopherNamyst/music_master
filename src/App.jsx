f5f0ff89cbbe6404787a7ce194ac3382b4f61879
Christopher Paul Namyst <cpnamyst79@gmail.com>
2017-12-13 18:22:14 -0500

    added some basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,55 @@
 import React, { Component } from 'react';
+import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  search() {
+    console.log('this.state', this.state);
+  }
   render() {
     return (
-      <div>
+      <div className="App">
+
         <div className="App-title">Music Master</div>
-        <div>
-          <input placeholder="search an artist..."/>
-          <button>button</button>
-        </div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for an Artist"
+              query={this.state.query}
+              onChange={event => {this.setState({query: event.target.value})}}
+                onKeyPress={event => {
+                  if(event.key === "Enter") {
+                    this.search()
+                  }
+                }}
+            />
+            <InputGroup.Addon onClick={() => this.search()}>
+              <Glyphicon glyph="search">
+
+              </Glyphicon>
+            </InputGroup.Addon>
+
+          </InputGroup>
+
+
+
+        </FormGroup>
         <div className="Profile">
+          <div>Artist Picture</div>
           <div>Artist Name</div>
         </div>
+        <div className="Gallery">
+          Gallery
+        </div>
+
       </div>
     )
   }
