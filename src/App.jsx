70a4232fa006ab212408908298bf18872a44fd51
Christopher Paul Namyst <cpnamyst79@gmail.com>
2017-12-13 15:49:43 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="App-title">Music Master</div>
+        <div>
+          <input placeholder="search an artist..."/>
+          <button>button</button>
+        </div>
+        <div className="Profile">
+          <div>Artist Name</div>
+        </div>
+      </div>
+    )
+  }
+
+}
+
+
+
+export default App;
